ed374300179359a88a94b1966c347eb0a67b9502
Peter <adhikarisuzal944@gmail.com>
2026-06-23 18:56:28 -0400

    all about next day variance

--- garch.py
+++ garch.py
@@ -5,8 +5,6 @@ class Garch:
         self.alpha = alpha ## How much we care about yesterday's shock. High alpha = market is jumpy, reacts hard to surprises. 
         self.beta = beta ## How much yesterday's volatility carries into today. High beta == volatility is persistent, slow to die down 
 
-    
-
     def computing_variance(self, returns):
         n = len(returns) ## This is the return of the n number of days. 
         variance = np.zeros(n) ## Setting up the variance to be zero  as the size of the returns
@@ -15,3 +13,8 @@ class Garch:
             variance[t] = self.omega + self.alpha * returns[t-1] **2 + self.beta * variance[t-1]
         return variance  ## It returns the list of variance where the index represents each day varaince, 
     
+    def forecast_next_day(self, returns, variance):
+        last_return = returns[-1]
+        last_variance = variance[-1]
+        next_day_variance = self.omega + self.alpha ** last_return **2 + self.beta * last_variance
+        return next_day_variance
