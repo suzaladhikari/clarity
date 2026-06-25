5cb43b2a28ff383a972a9557bd4a7b6c3cea282b
Peter <adhikarisuzal944@gmail.com>
2026-06-25 08:43:41 -0400

    return , log_return and squared log returns

--- src/data_pipeline/featureengineering.py
+++ src/data_pipeline/featureengineering.py
@@ -4,5 +4,8 @@ import numpy as np
 
 ### Here we will be creating new features 
 
-def feature_creation():
-    pass
+def feature_creation(data):
+    ## Creating the log return 
+    data['return'] = data['adjClose'].pct_change() ## this gives the return 
+    data['log_return'] = np.log(data['adjClose'] / data['adjClose'].shift(1)) ## Log returns 
+    data['squred_log_returns'] = data['log_return'] ** 2
