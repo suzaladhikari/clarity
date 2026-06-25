eb392f7bbc6294e29e1065aefa4e65292952e48c
Peter <adhikarisuzal944@gmail.com>
2026-06-25 09:22:51 -0400

    only log_returns, ewma_vol_20, volume z score, dropna return

--- src/data_pipeline/featureengineering.py
+++ src/data_pipeline/featureengineering.py
@@ -6,15 +6,23 @@ import numpy as np
 
 def feature_creation(data):
     ## Creating the log return 
-    data['return'] = data['adjClose'].pct_change() ## this gives the return 
     data['log_return'] = np.log(data['adjClose'] / data['adjClose'].shift(1)) ## Log returns 
     data['squred_log_returns'] = data['log_return'] ** 2
     data['abs_log_returns'] = np.abs(data['log_return'])
+    data['ewma_vol_20'] = data['log_return'].ewm(span=20).std()
 
     numbers = [5,10,20,30]
     for num in numbers:
         column = f'rolling_vol_{num}'
-        data[column] = data['log_return'].rolling(num).std()
+        data[column] = data['log_return'].rolling(num).std() ## Certain window volatility 
+    
 
-# Calculating the daily range volatilitiy 
     data['daily_range'] = (data['adjHigh'] - data['adjLow'])/data['adjClose'] ## Calculating intraday changes/turbulence.
+
+    ## Volume shock 
+    volume_mean = data['adjVolume'].rolling(20).mean()
+    volume_std = data['adjVolume'].rolling(20).mean()
+
+    data['volume_zscore'] = (data['volume'] - volume_mean) / volume_std
+
+    return data.dropna()
