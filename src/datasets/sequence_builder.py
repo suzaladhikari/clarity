43e3ac153ab37f91847ce50f287c8279969e0733
Peter <adhikarisuzal944@gmail.com>
2026-06-26 09:27:59 -0400

    columns dropped

--- src/datasets/sequence_builder.py
+++ src/datasets/sequence_builder.py
@@ -18,18 +18,20 @@ for filename in os.listdir(combined_directory):
 combined_stock_data = pd.concat(combined_data, ignore_index=True)
 print(combined_stock_data.shape[0])
 
+## Dropping the unnecessary columns 
+columns_to_drop = ['close', 'high', 'low', 'open', 'volume', 'adjClose', 'adjHigh','adjLow', 'adjOpen', 'adjVolume', 'divCash', 'splitFactor',]
+combined_stock_data = combined_stock_data.drop(columns = columns_to_drop)
 
 ## Doing the train, validation and test split with the date
-train_split = combined_stock_data[combined_stock_data['date'] < '2020-01-01']
-validation_split = combined_stock_data[(combined_stock_data['date'] >= '2020-01-01' ) & (combined_stock_data['date'] < '2022-01-01')]
-test_split = combined_stock_data[combined_stock_data['date'] >= '2022-01-01']
+train_split = combined_stock_data[combined_stock_data['date'] < '2020-01-01'].copy()
+validation_split = combined_stock_data[(combined_stock_data['date'] >= '2020-01-01' ) & (combined_stock_data['date'] < '2022-01-01')].copy()
+test_split = combined_stock_data[combined_stock_data['date'] >= '2022-01-01'].copy()
 print(train_split.shape, validation_split.shape, test_split.shape)
 
 ## Scaling the features
-columns_to_drop = ['close', 'high', 'low', 'open', 'volume', 'adjClose', 'adjHigh','adjLow', 'adjOpen', 'adjVolume', 'divCash', 'splitFactor',]
-combined_stock_data = combined_stock_data.drop(columns = columns_to_drop)
+
 scaler = StandardScaler()
 scalable_columns = combined_stock_data.drop(columns = ['date', 'Symbol', 'target_volatility']).columns
 train_split[scalable_columns] = scaler.fit_transform(train_split[scalable_columns])
-validation_split[scalable_columns] = scaler.transform(train_split[scalable_columns])
-test_split[scalable_columns] = scaler.transform(train_split[scalable_columns])
+validation_split[scalable_columns] = scaler.transform(validation_split[scalable_columns])
+test_split[scalable_columns] = scaler.transform(test_split[scalable_columns])
