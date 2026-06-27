b2a0f631908d35801975badf9b684843549a1e2f
Peter <adhikarisuzal944@gmail.com>
2026-06-27 09:17:52 -0400

    conversion of X_val, y_val, X_test, and y_test

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -5,3 +5,10 @@ from sequence_builder import X_train, y_train, X_test, y_test, X_val, y_val
 
 X_train= torch.FloatTensor(X_train)
 y_train= torch.FloatTensor(y_train)
+
+X_val = torch.FloatTensor(X_val)
+y_val = torch.FloatTensor(y_val)
+
+X_test = torch.FloatTensor(X_test)
+y_test = torch.FloatTensor(y_test)
+
