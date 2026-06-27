8a52346d06d9795147e19e6cf3833ef26994918e
Peter <adhikarisuzal944@gmail.com>
2026-06-27 09:20:14 -0400

    same for val and test data

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -14,3 +14,6 @@ y_test = torch.FloatTensor(y_test)
 
 ### Creating the tensor dataset
 train_dataset = TensorDataset(X_train, y_train)
+validation_datset = TensorDataset(X_val, y_val)
+test_dataset = TensorDataset(X_test, y_test)
+
