18735435c237cc1263e54dfec47f75d55bd9d314
Peter <adhikarisuzal944@gmail.com>
2026-06-27 09:16:51 -0400

    converted to float tensor

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import torch 
 from torch.utils.data import DataLoader, TensorDataset
 from sequence_builder import X_train, y_train, X_test, y_test, X_val, y_val 
 
+X_train= torch.FloatTensor(X_train)
+y_train= torch.FloatTensor(y_train)
