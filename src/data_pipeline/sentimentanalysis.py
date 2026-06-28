7d38e38bc61951d1a6e7d3db75879774aaa3234f
Peter <adhikarisuzal944@gmail.com>
2026-06-28 19:54:33 -0400

    alrighty here we go

--- src/data_pipeline/sentimentanalysis.py
+++ src/data_pipeline/sentimentanalysis.py
@@ -1,6 +1,8 @@
 ## Sentiment analysis from the FinBERT 
 from transformers import pipeline, BertTokenizer, BertForSequenceClassification
 import requests
+import pandas as pd 
+from ingest import watchlist
 
 tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert") ## Used to tokenize 
 model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert") ## For classification of the tokens
@@ -34,5 +36,29 @@ def score_headline(headline, threshold = 0.70):
     else:
         return 0.0
     
+def build_daily_sentiment(ticker, start_date, api_key):
+    articles = fetch_news(ticker, start_date, api_key)
+    if not articles:
+        return pd.DataFrame(columns = ['date', 'sentiment'])
+
+    records = []
+    for article in articles:
+        ## Storing the date of the article published
+        date = pd.to_datetime(article['publishedDate']).date() ## date of published
+        headline = article.get('title', '') ## Storing the title only, thats all we need
+        score = score_headline(article) ## Calculating the score
+        records.append({'date':date, 'sentiment': score})
+
+    data = pd.DataFrame(records)
+
+    ## Averaging each day's headlines scores and making it one score per day 
+    daily = data.groupby('date')['sentiment'].mean().reset_index()
+    daily['date'] = pd.to_datetime(daily['date'])
+
+    return daily
 
 
+
+
+for stock in watchlist:
+    build_daily_sentiment(stock, '2008-01-01','8e14d5babfe29c8815f268eb1afa1727ce18f16e' )
