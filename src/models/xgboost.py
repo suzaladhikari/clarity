423749d8d189c24a5dfa6493ef6437200b67ba3d
Peter <adhikarisuzal944@gmail.com>
2026-06-28 07:51:42 -0400

    xgboost model ready

---
+++ src/models/xgboost.py
@@ -0,0 +1,18 @@
+import xgboost as xgb
+
+
+def build_xgboost_model(params = None, early_stopping_rounds = 50):
+    default_params = {
+        'n_estimators': 200, 
+        'max_depth': 6, 
+        'learning_rate':0.05,
+        'subsample':0.8, ## prevents overfitting
+        'colsample_bytree':0.8, ## feature subsampling 
+        'objective':'reg:squarederror', ## used for regression 
+        'randomstate':42, ## Setting up the random seed
+        'n_jobs': -1  ## Using all the cores
+    }
+    if params:
+        default_params.update(params)
+    
+    return xgb.XGBRegressor(**default_params, early_stopping_rounds = early_stopping_rounds)
