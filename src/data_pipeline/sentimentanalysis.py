8b688f33b842f96896c069c1c15f995e87b29bc7
Peter <adhikarisuzal944@gmail.com>
2026-06-28 19:25:59 -0400

    fetching out the response

--- src/data_pipeline/sentimentanalysis.py
+++ src/data_pipeline/sentimentanalysis.py
@@ -1,6 +1,18 @@
 ## Sentiment analysis from the FinBERT 
 from transformers import pipeline, BertTokenizer, BertForSequenceClassification
+import requests
 
-tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert")
-model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert")
-sentiment_pipeline = pipeline("sentiment-analysis", model = model, tokenizer = tokenizer)
+tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert") ## Used to tokenize 
+model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert") ## For classification of the tokens
+sentiment_pipeline = pipeline("sentiment-analysis", model = model, tokenizer = tokenizer) ## Pipeline to do the process
+
+def fetch_news(ticker, start_date, end_date, api_key):
+    url = "https://api.tiingo.com/tiingo/news"
+    params = {
+        "tickers":ticker,
+        "startDate": start_date, 
+        "endDate":end_date, 
+        "token": api_key
+    }
+    response = requests.get(url, params= params)
+    return response.json()
