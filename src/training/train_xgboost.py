4296fc06ee139ef7c9ea7a3760ce184a9be003b1
Peter <adhikarisuzal944@gmail.com>
2026-06-28 09:10:02 -0400

    done with the loading data

--- src/training/train_xgboost.py
+++ src/training/train_xgboost.py
@@ -13,20 +13,23 @@ combined_stock_data = pd.read_parquet('./datas/features/combined_data.parquet')
 train_split = combined_stock_data[combined_stock_data['date'] < '2020-01-01'].copy()
 validation_split = combined_stock_data[(combined_stock_data['date'] >= '2020-01-01' ) & (combined_stock_data['date'] < '2022-01-01')].copy()
 test_split = combined_stock_data[combined_stock_data['date'] >= '2022-01-01'].copy()
-
+DROP_COLS = ['date', 'Symbol', 'target_volatility']
+TARGET = 'target_volatility'
 ## Scaling the features
 
 scaler = StandardScaler()
-scalable_columns = combined_stock_data.drop(columns = ['date', 'Symbol', 'target_volatility']).columns
+scalable_columns = [c for c in combined_stock_data.columns if c not in DROP_COLS]
 train_split[scalable_columns] = scaler.fit_transform(train_split[scalable_columns])
 validation_split[scalable_columns] = scaler.transform(validation_split[scalable_columns])
 test_split[scalable_columns] = scaler.transform(test_split[scalable_columns])
 
-X_train = train_split.iloc[:, :-1]  
-y_train = train_split.iloc[:, -1]
+X_train = train_split[scalable_columns] 
+y_train = train_split[TARGET]
 
-X_val = validation_split.iloc[:, :-1]
-y_val = validation_split.iloc[:, -1]
+X_val = validation_split[scalable_columns]
+y_val = validation_split[TARGET]
 
-X_test = test_split.iloc[:, :-1]
-y_test = test_split.iloc[:, -1]
+X_test = test_split[scalable_columns]
+y_test = test_split[TARGET]
+
+## Loading data is done ! 
