f74ae65732ec2f42015816f0f5ced2fbfbcab4a5
Peter <adhikarisuzal944@gmail.com>
2026-06-29 09:32:48 -0400

    url and headers done

--- src/data_pipeline/sentimentanalysis.py
+++ src/data_pipeline/sentimentanalysis.py
@@ -3,3 +3,9 @@ from transformers import pipeline, BertTokenizer, BertForSequenceClassification
 import requests
 import pandas as pd 
 from bs4 import BeautifulSoup
+
+def get_fed_url(start_date = 2008):
+    url = "https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm"
+    headers = {"User-Agent":"Mozilla/5.0"} ## Just letting the website know who is visitng the page just for security purpose
+
+    
