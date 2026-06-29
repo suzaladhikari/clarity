a7d80f07bfe22ef246414bc133459afe4e5e4365
Peter <adhikarisuzal944@gmail.com>
2026-06-29 14:00:34 -0400

    features and samples to device

--- src/training/train_dl.py
+++ src/training/train_dl.py
@@ -3,5 +3,13 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from src.models.rnn import RNN
 from src.datasets.dataset_loader import train_loader, validation_loader, test_loader
+import torch 
+model = RNN(16, 32)
 
-print(train_loader)
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+epochs = 20
+for epoch in range(epochs):
+    for batch in train_loader:
+        samples, features = batch  
+        samples = samples.to(device)
+        features = features.to(device)
