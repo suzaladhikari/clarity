6c7d8060fada5bc367e2daeca9bf919af530875d
Peter <adhikarisuzal944@gmail.com>
2026-06-29 09:36:20 -0400

    extracting out frombeautiful soup

--- src/data_pipeline/sentimentanalysis.py
+++ src/data_pipeline/sentimentanalysis.py
@@ -8,4 +8,7 @@ def get_fed_url(start_date = 2008):
     url = "https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm"
     headers = {"User-Agent":"Mozilla/5.0"} ## Just letting the website know who is visitng the page just for security purpose
 
+    response = requests.get(url, headers = headers)
+    soup = BeautifulSoup(response.text, 'html.parser') ## Extracting out the useful text from the parser
+
     
