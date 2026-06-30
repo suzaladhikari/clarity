55739cd0c4c0ed24e458687df51cc88f9901edd8
Peter <adhikarisuzal944@gmail.com>
2026-06-30 09:38:21 -0400

    used the utils in some way

--- src/training/train_dl.py
+++ src/training/train_dl.py
@@ -5,6 +5,7 @@ from src.models.rnn import RNN
 from src.datasets.dataset_loader import train_loader, validation_loader, test_loader
 import torch 
 import torch.nn as nn
+from src.training.trainer_utils import model_train_and_validate
 
 ## Setting up the device
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -17,35 +18,8 @@ model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
 loss_function = nn.MSELoss()
 
-for epoch in range(epochs):
-    model.train()
-    total_loss = 0 
-    for batch in train_loader:
-        optimizer.zero_grad()
-        samples, features = batch  
-        samples = samples.to(device)
-        features = features.to(device)
-        output = model(samples)
-        loss = loss_function(output, features)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
+## Getting the training and validation loss 
+train_loss, validation_loss = model_train_and_validate(model, epochs, train_loader, validation_loader, optimizer, loss_function, device)
 
-    print(f"Epoch {epoch+1}/{epochs}, Loss -> {total_loss/len(train_loader.dataset) :.6f}")
-
-
-    model.eval()
-    validation_loss = 0
-    with torch.no_grad():
-        for batch in validation_loader: 
-
-            samples, features = batch 
-            samples = samples.to(device)
-            features = features.to(device)
-            output = model(samples)
-            loss = loss_function(output, features)
-            validation_loss += loss.item()
-        
-        print(f"Epoch {epoch+1}/{epochs}, Loss -> {validation_loss/len(validation_loader.dataset) :.6f}")
-
+print(train_loss, validation_loss)
 
