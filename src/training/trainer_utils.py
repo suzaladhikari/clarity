5fc0c9e630260b05b9cf979bf9a2b0fd64d4bd94
Peter <adhikarisuzal944@gmail.com>
2026-06-30 09:34:33 -0400

    did the model training and validation

--- src/training/trainer_utils.py
+++ src/training/trainer_utils.py
@@ -2,7 +2,7 @@
 import torch 
 import torch.nn as nn 
 
-def model_trainer(model,epoches, train_loader, validation_loader, optimizer, loss_method, device):
+def model_train_and_validate(model,epoches, train_loader, validation_loader, optimizer, loss_method, device):
     for epoch in range(epoches):
         model.train()
         train_loss = 0
@@ -17,3 +17,15 @@ def model_trainer(model,epoches, train_loader, validation_loader, optimizer, los
             optimizer.step()
             train_loss += loss.item()
         print(f"{epoch}/{epoches} , Training_loss:{train_loss/len(train_loader.dataset)}")
+
+        model.eval()
+        validation_loss = 0
+        with torch.no_grad():
+            for batch in validation_loader:
+                samples, features = batch 
+                samples.to(device)
+                features.to(device)
+                output = model(samples)
+                loss = loss_method(output, features)
+                validation_loss += loss.item()
+            print(f"{epoch}/{epoches} , Training_loss:{validation_loss/len(validation_loader.dataset)}")
