62c46d9d042095422bf84490d33f04bef4eb4604
Peter <adhikarisuzal944@gmail.com>
2026-06-30 09:31:19 -0400

    training loss calculated

---
+++ src/training/trainer_utils.py
@@ -0,0 +1,19 @@
+## Creating the traiing and testing function automatically 
+import torch 
+import torch.nn as nn 
+
+def model_trainer(model,epoches, train_loader, validation_loader, optimizer, loss_method, device):
+    for epoch in range(epoches):
+        model.train()
+        train_loss = 0
+        for batch in train_loader:
+            optimizer.zero_grad()
+            samples, features = batch 
+            samples.to(device)
+            features.to(device)
+            output = model(samples)
+            loss = loss_method(output, features)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+        print(f"{epoch}/{epoches} , Training_loss:{train_loss/len(train_loader.dataset)}")
