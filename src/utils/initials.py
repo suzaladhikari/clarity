24fb614bd1c01c46978a79f5c9070665ad6c1e08
Peter <adhikarisuzal944@gmail.com>
2026-07-01 08:33:55 -0400

    loading pickle done

--- src/utils/initials.py
+++ src/utils/initials.py
@@ -5,3 +5,10 @@ def saving_pickle(path, object):
         pickle.dump(object, f)
 
 ## Loading the model 
+
+## Loading the pickle file 
+
+def loading_pickle(path):
+    with open(path, 'rb') as f:
+        obj = pickle.load(f)
+    return obj 
