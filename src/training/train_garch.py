fe42b0130b8bf3db69f3895725e30913c4b29ba5
Peter <adhikarisuzal944@gmail.com>
2026-07-03 08:48:24 -0400

    fixed the garch model

--- src/training/train_garch.py
+++ src/training/train_garch.py
@@ -1,13 +1,17 @@
 from arch import arch_model 
+import sys 
+import os 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import numpy as np
 import pandas as pd 
+from src.models.garch import Garch
 ### Data 
 data = pd.read_parquet('./datas/features/combined_data.parquet')
 
 training_data = data[data['date'] < '2022-01-01'].copy()
 testing_data = data[data['date'] >= '2022-01-01'].copy()
 
-model = arch_model(training_data['log_return'])
+model = arch_model(training_data['log_return'] * 100, vol = 'Garch', p = 1, q = 1)
 results = model.fit(disp = 'off') ## Used this to get the cleaner output
 
 ### Extracting the omega, alpha and beta
@@ -15,4 +19,7 @@ omega = results.params['omega']
 alpha = results.params['alpha[1]']
 beta = results.params['beta[1]']
 
+## Garch model 
+garch = Garch(omega, alpha, beta )
 
+print(garch )
