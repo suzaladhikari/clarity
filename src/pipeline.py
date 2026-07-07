b36aa482dccf9952c66fe196365d90f600727cf4
Peter <adhikarisuzal944@gmail.com>
2026-07-07 10:55:58 -0400

    did the xgboost model

--- src/pipeline.py
+++ src/pipeline.py
@@ -11,6 +11,8 @@ from src.datasets.sequence_builder import train_test_val
 from src.datasets.dataset_loader import creating_data_loaders
 from src.training.train_lstm import lstm_model
 from src.training.train_rnn import rnn_model 
+from src.training.train_xgboost import xgboost_model
+
 def extracting_to_final_data(key):
     correctedData = data_corrector(key)
     feature_engineering = feature_creation(correctedData)
@@ -30,6 +32,9 @@ train_loader, validation_loader, test_loader = creating_data_loaders(X_train, y_
 ## Models 
 lstm_model_results = lstm_model(train_loader, validation_loader, test_loader)
 rnn_model_results = rnn_model(train_loader, validation_loader, test_loader)
+xgboost_resutls = xgboost_model(combined_stock_data)
+
+
 ## Ani tes pachi make sure you save the whole files in the dictionary ! got it 
 ## Ani __name__ == __main__ ko use ni k huncha bhanera bujne 
 ## Ani make sure you create the reusable function in seqeucen builder and also the dataloader to carry on your project
