a30254b56294db773da27538179429be983550ac
Peter <adhikarisuzal944@gmail.com>
2026-07-06 21:42:20 -0400

    alright its working now

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,12 +3,11 @@
 import sys 
 import os 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.data_pipeline.ingest import company_datasets ## Function to extract the company dataset 
+from src.data_pipeline.ingest import watchlist ## Function to extract the company dataset 
 from src.data_pipeline.cleaner import data_corrector ## to correct and check the nans and infs
 from src.data_pipeline.featureengineering import feature_creation ## Creating new features
 def extracting_to_final_data(key):
-    rawdata = company_datasets(key)
-    correctedData = data_corrector(rawdata)
+    correctedData = data_corrector(key)
     feature_engineering = feature_creation(correctedData)
     return feature_engineering
 
