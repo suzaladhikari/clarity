2e80f4b43633012f27c825f5fb77623a67ca2e6b
Peter <adhikarisuzal944@gmail.com>
2026-07-07 09:43:43 -0400

    data loading done

--- src/pipeline.py
+++ src/pipeline.py
@@ -8,6 +8,7 @@ from src.data_pipeline.ingest import watchlist ## Function to extract the compan
 from src.data_pipeline.cleaner import data_corrector ## to correct and check the nans and infs
 from src.data_pipeline.featureengineering import feature_creation ## Creating new features
 from src.datasets.sequence_builder import train_test_val
+from src.datasets.dataset_loader import creating_data_loaders
 def extracting_to_final_data(key):
     correctedData = data_corrector(key)
     feature_engineering = feature_creation(correctedData)
@@ -22,10 +23,9 @@ combined_stock_data = pd.concat(combined_data, ignore_index=True)
 
 ## Creating the sequences out of the combined datasets
 X_train, y_train, X_val, y_val, X_test, y_test = train_test_val(combined_stock_data)
+train_loader, validation_loader, test_loader = creating_data_loaders(X_train, y_train, X_val, y_val, X_test, y_test)
 
-## Building the sequences
 
-### Pipeline ma chai aaba seqeucne ani dataloader ko finalize garne 
 ## Ani tes pachi make sure you save the whole files in the dictionary ! got it 
 ## Ani __name__ == __main__ ko use ni k huncha bhanera bujne 
 ## Ani make sure you create the reusable function in seqeucen builder and also the dataloader to carry on your project
