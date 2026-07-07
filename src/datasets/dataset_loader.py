45ca4a3a0c8dc1223221f29f339270ee764eb8b3
Peter <adhikarisuzal944@gmail.com>
2026-07-07 09:42:56 -0400

    created the func to create data loader obj

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -21,23 +21,25 @@ torch.backends.cudnn.benchmark = False
 ### Creating a generator to pass into the data loader 
 g = torch.Generator()
 g.manual_seed(RANDOMSEED)
+def creating_data_loaders(X_train, y_train, X_val, y_val, X_test, y_test):
+    X_train= torch.FloatTensor(X_train)
+    y_train= torch.FloatTensor(y_train).view(-1,1) ## Adding the dimension to the input
 
-X_train= torch.FloatTensor(X_train)
-y_train= torch.FloatTensor(y_train).view(-1,1) ## Adding the dimension to the input
+    X_val = torch.FloatTensor(X_val)
+    y_val = torch.FloatTensor(y_val).view(-1,1)
 
-X_val = torch.FloatTensor(X_val)
-y_val = torch.FloatTensor(y_val).view(-1,1)
+    X_test = torch.FloatTensor(X_test)
+    y_test = torch.FloatTensor(y_test).view(-1,1)
 
-X_test = torch.FloatTensor(X_test)
-y_test = torch.FloatTensor(y_test).view(-1,1)
+    ### Creating the tensor dataset
+    train_dataset = TensorDataset(X_train, y_train)
+    validation_dataset = TensorDataset(X_val, y_val)
+    test_dataset = TensorDataset(X_test, y_test)
 
-### Creating the tensor dataset
-train_dataset = TensorDataset(X_train, y_train)
-validation_dataset = TensorDataset(X_val, y_val)
-test_dataset = TensorDataset(X_test, y_test)
+    ### Creating the data loader
 
-### Creating the data loader
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle = True, generator=g)
+    validation_loader = DataLoader(validation_dataset, batch_size=128, shuffle=False )
+    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle = True, generator=g)
-validation_loader = DataLoader(validation_dataset, batch_size=128, shuffle=False )
-test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+    return train_loader, validation_loader, test_loader
