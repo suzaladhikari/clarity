3e54a88d860ee295b2ffa194a886a8581f1d3c53
Peter <adhikarisuzal944@gmail.com>
2026-07-06 21:19:59 -0400

    fixed the path issue

--- src/data_pipeline/featureengineering.py
+++ src/data_pipeline/featureengineering.py
@@ -1,6 +1,4 @@
-import sys 
-import os 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
 import pandas as pd
 import os 
 import numpy as np
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,9 +1,8 @@
 ## Creating the pipeline so that I donot have to rerun the code over again and again 
 ## Necessary imports 
-
 import sys 
 import os 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.data_pipeline.ingest import company_datasets ## Function to extract the company dataset 
 from src.data_pipeline.cleaner import data_corrector ## to correct and check the nans and infs
 from src.data_pipeline.featureengineering import feature_creation ## Creating new features
@@ -13,3 +12,5 @@ def extracting_to_final_data(key):
     feature_engineering = feature_creation(correctedData)
     return feature_engineering
 
+data = extracting_to_final_data("AAPL")
+data.head(5)
