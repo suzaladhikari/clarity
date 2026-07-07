0e838303d4a2844f680eb055126dfe2568ce0397
Peter <adhikarisuzal944@gmail.com>
2026-07-06 21:10:01 -0400

    data corrector imported

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,8 +5,9 @@ import sys
 import os 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from src.data_pipeline.ingest import company_datasets ## Function to extract the company dataset 
-
+from src.data_pipeline.cleaner import data_corrector ## to correct and check the nans and infs
 
 def extracting_to_final_data(key):
     rawdata = company_datasets(key)
+
 
