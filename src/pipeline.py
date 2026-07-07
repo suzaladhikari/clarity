feb38d20d5a1364d0a9ad03e593bb45a02ee3d22
Peter <adhikarisuzal944@gmail.com>
2026-07-06 21:16:22 -0400

    added feature engineering

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,8 +6,10 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from src.data_pipeline.ingest import company_datasets ## Function to extract the company dataset 
 from src.data_pipeline.cleaner import data_corrector ## to correct and check the nans and infs
-
+from src.data_pipeline.featureengineering import feature_creation ## Creating new features
 def extracting_to_final_data(key):
     rawdata = company_datasets(key)
-
+    correctedData = data_corrector(rawdata)
+    feature_engineering = feature_creation(correctedData)
+    return feature_engineering
 
