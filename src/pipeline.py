7d93938aa9db611ce1e836034e65265e396f8165
Peter <adhikarisuzal944@gmail.com>
2026-07-08 11:55:57 -0400

    fixed the issues and transferred the model's performance as a csv

--- src/pipeline.py
+++ src/pipeline.py
@@ -13,35 +13,41 @@ from src.training.train_lstm import lstm_model
 from src.training.train_rnn import rnn_model 
 from src.training.train_xgboost import xgboost_model
 from src.training.train_garch import garch_model
-def extracting_to_final_data(key):
-    correctedData = data_corrector(key)
-    feature_engineering = feature_creation(correctedData)
-    return feature_engineering
+def main():
+    def extracting_to_final_data(key):
+        correctedData = data_corrector(key)
+        feature_engineering = feature_creation(correctedData)
+        return feature_engineering
 
-combined_data = []
-for ticker in watchlist.keys():
-    stock_data = extracting_to_final_data(ticker)
-    combined_data.append(stock_data)
+    combined_data = []
+    for ticker in watchlist.keys():
+        stock_data = extracting_to_final_data(ticker)
+        combined_data.append(stock_data)
 
-combined_stock_data = pd.concat(combined_data, ignore_index=True)
+    combined_stock_data = pd.concat(combined_data, ignore_index=True)
 
-## Creating the sequences out of the combined datasets
-X_train, y_train, X_val, y_val, X_test, y_test = train_test_val(combined_stock_data)
-train_loader, validation_loader, test_loader = creating_data_loaders(X_train, y_train, X_val, y_val, X_test, y_test)
+    ## Creating the sequences out of the combined datasets
+    X_train, y_train, X_val, y_val, X_test, y_test = train_test_val(combined_stock_data)
+    train_loader, validation_loader, test_loader = creating_data_loaders(X_train, y_train, X_val, y_val, X_test, y_test)
 
-## Models 
-lstm_model_results = lstm_model(train_loader, validation_loader, test_loader)
-rnn_model_results = rnn_model(train_loader, validation_loader, test_loader)
-xgboost_resutls = xgboost_model(combined_stock_data)
-garch_results = garch_model(combined_stock_data)
+    ## Models 
+    lstm_model_results = lstm_model(train_loader, validation_loader, test_loader)
+    rnn_model_results = rnn_model(train_loader, validation_loader, test_loader)
+    xgboost_resutls = xgboost_model(combined_stock_data)
+    garch_results = garch_model(combined_stock_data)
 
-combined_results = {}
-combined_results['mae'] = [lstm_model_results['mae'], rnn_model_results['mae'], xgboost_resutls['mae'], garch_results['mae']]
-combined_results['rmse'] = [lstm_model_results['rmse'], rnn_model_results['rmse'], xgboost_resutls['rmse'], garch_results['rmse']]
-combined_results['r2'] = [lstm_model_results['r2'], rnn_model_results['r2'], xgboost_resutls['r2'], garch_results['r2']]
+    combined_results = {}
+    combined_results['mae'] = [lstm_model_results['mae'], rnn_model_results['mae'], xgboost_resutls['mae'], garch_results['mae']]
+    combined_results['rmse'] = [lstm_model_results['rmse'], rnn_model_results['rmse'], xgboost_resutls['rmse'], garch_results['rmse']]
+    combined_results['r2'] = [lstm_model_results['r2'], rnn_model_results['r2'], xgboost_resutls['r2'], garch_results['r2']]
 
 
-models_performance = pd.DataFrame(combined_results)
+    models_performance = pd.DataFrame(combined_results)
+    return models_performance
 
-### Scaler feri use gar hai ta like reusable scaler use garne isn all the models 
+if __name__ == '__main__':
+    results = main()
+    print(results)
+    results.to_csv('../datas/modelperformance.csv', index = False)
+
 ### ani tespachi plot the necessary pictures to evaluate the model
