571df01c5c30b0392d5659ece9c94b598dbd6199
Peter <adhikarisuzal944@gmail.com>
2026-07-09 11:01:02 -0400

    removed unnecessary y_pred and true values stuff for both garch and xgboost

--- src/training/train_xgboost.py
+++ src/training/train_xgboost.py
@@ -52,8 +52,6 @@ def xgboost_model(combined_stock_data):
     y_pred, rmse, mae,r2, true_values= evaluate_model(model, X_test, y_test)
 
     xgboost_results = {}
-    xgboost_results['y_pred'] = y_pred.tolist()
-    xgboost_results['true_labels'] = true_values.tolist()
     xgboost_results['mae'] = mae
     xgboost_results['rmse'] = rmse
     xgboost_results['r2'] = r2
