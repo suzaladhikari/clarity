bded507d012b606032ba503b27fca41ec3579d8e
Peter <adhikarisuzal944@gmail.com>
2026-07-09 10:41:25 -0400

    excluded garch

--- src/pipeline.py
+++ src/pipeline.py
@@ -11,8 +11,6 @@ from src.datasets.sequence_builder import train_test_val
 from src.datasets.dataset_loader import creating_data_loaders
 from src.training.train_lstm import lstm_model
 from src.training.train_rnn import rnn_model 
-# from src.training.train_xgboost import xgboost_model
-from src.training.train_garch import garch_model
 def extracting_to_final_data(key):
     correctedData = data_corrector(key)
     feature_engineering = feature_creation(correctedData)
@@ -33,13 +31,12 @@ train_loader, validation_loader, test_loader = creating_data_loaders(X_train, y_
     ## Models 
 lstm_model_results = lstm_model(train_loader, validation_loader, test_loader)
 rnn_model_results = rnn_model(train_loader, validation_loader, test_loader)
-# xgboost_resutls = xgboost_model(combined_stock_data)
-garch_results = garch_model(combined_stock_data)
+
 
 combined_results = {}
-combined_results['mae'] = [lstm_model_results['mae'], rnn_model_results['mae'], garch_results['mae']]
-combined_results['rmse'] = [lstm_model_results['rmse'], rnn_model_results['rmse'], garch_results['rmse']]
-combined_results['r2'] = [lstm_model_results['r2'], rnn_model_results['r2'], garch_results['r2']]
+combined_results['mae'] = [lstm_model_results['mae'], rnn_model_results['mae']]
+combined_results['rmse'] = [lstm_model_results['rmse'], rnn_model_results['rmse']]
+combined_results['r2'] = [lstm_model_results['r2'], rnn_model_results['r2']]
 
 
 models_performance = pd.DataFrame(combined_results)
