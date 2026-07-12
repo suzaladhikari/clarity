039c5256370edf6301dd54cf50ffb8385704d0ad
Peter <adhikarisuzal944@gmail.com>
2026-07-12 18:26:31 -0400

    the watchlist import is working

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,3 +1,13 @@
 ### What data are allowed to enter and leave the api !
+import sys, os
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../..")
+    )
+)
 from pydantic import BaseModel 
-print(BaseModel)
+from src.data_pipeline.ingest import watchlist
+
+class PredictionRequest(BaseModel):
+    pass
+print(watchlist)
