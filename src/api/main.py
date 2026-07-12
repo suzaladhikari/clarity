e1df737ea374d3224ccff615bf0b8ba624946325
Peter <adhikarisuzal944@gmail.com>
2026-07-12 09:23:58 -0400

    updated info

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI 
 
 app = FastAPI(title = "API for Clarity", description= "API for stock volatility prediction", version = "1.0.0")
-
-
-## Creating the elements 
-@app.get('/hello')
+#uvicorn src.api.main:app --reload
+# Basic information of the api  
+@app.get('/info')
 def say_hello():
-    return "Hey There"
+    return "Through Clarity API you can predict the volatility for 20 different stocks using four different models: LSTM, RNN, GARCH, or XGBoost"
