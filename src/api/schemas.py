34a6ef2a1253f038ad7af95ad8aef6ced106fd66
Peter <adhikarisuzal944@gmail.com>
2026-07-12 18:32:31 -0400

    making sure the stock user select are in our goddamn file

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,13 +1,14 @@
 ### What data are allowed to enter and leave the api !
 import sys, os
-sys.path.append(
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "../..")
-    )
-)
-from pydantic import BaseModel 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from pydantic import BaseModel, field_validator
 from src.data_pipeline.ingest import watchlist
-
 class PredictionRequest(BaseModel):
-    pass
-print(watchlist)
+    ticker : str 
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls,value):
+        if value not in watchlist.keys():
+            raise ValueError(f"{value} is not in the list of stocks data we have. Choost from {list(watchlist.keys())} we have")
+        return value 
+    
