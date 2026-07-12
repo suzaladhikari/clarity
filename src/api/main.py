6577d8ef24f3541da579308f75dd16524eb74e0b
Peter <adhikarisuzal944@gmail.com>
2026-07-12 09:18:53 -0400

    added hello

--- src/api/main.py
+++ src/api/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI 
 
 app = FastAPI(title = "API for Clarity", description= "API for stock volatility prediction", version = "1.0.0")
+
+
+## Creating the elements 
+@app.get('/hello')
+def say_hello():
+    return "Hey There"
