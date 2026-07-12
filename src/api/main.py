1760a14bc249e5ad34ab351a79cabb9726b9ec4c
Peter <adhikarisuzal944@gmail.com>
2026-07-12 09:40:39 -0400

    registering the routers

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI 
-
+from src.api.routes import router
 app = FastAPI(title = "API for Clarity", description= "API for stock volatility prediction", version = "1.0.0")
 #uvicorn src.api.main:app --reload
-# Basic information of the api  
+# Creating the routers 
 @app.get('/info')
 def say_hello():
     return "Through Clarity API you can predict the volatility for 20 different stocks using four different models: LSTM, RNN, GARCH, or XGBoost"
+
+## Registering all the routers created in routes.py 
+app.include_router(router)
