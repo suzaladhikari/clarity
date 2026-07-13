131db13bfba90c97df09ec18032582db945fe41a
Peter <adhikarisuzal944@gmail.com>
2026-07-13 09:38:01 -0400

    omega, alpha, and beta extracted

--- src/api/predictivemodels.py
+++ src/api/predictivemodels.py
@@ -1,5 +1,6 @@
-
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-from src.models.garch import Garch
-print(Garch)
+from src.training.train_garch import params_garch
+
+print(params_garch)
+
