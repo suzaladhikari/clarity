c700031c12a6025330ebc4a0d982ae215785ab37
Peter <adhikarisuzal944@gmail.com>
2026-07-13 09:50:45 -0400

    extracted the params

--- src/api/predictivemodels.py
+++ src/api/predictivemodels.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 from src.training.train_garch import params_garch
 from src.models.garch import Garch
 import pandas as pd
-
+omega, alpha, beta = params_garch['omega'], params_garch['alpha'], params_garch['beta']
 def return_value_by_ticker(ticker, datapath = './datas/combined_data.parquet'):
      data = pd.read_parquet(datapath)
      ticker_data = data[data['Symbol'] == ticker].sort_values(by = 'date').reset_index(drop = True)
@@ -11,3 +11,5 @@ def return_value_by_ticker(ticker, datapath = './datas/combined_data.parquet'):
         raise ValueError(f"There is no data of symbol : {ticker}")
      returns = ticker_data['log_return'].values * 100 
      return returns 
+
+def predict_next_day_volatility(returns):
