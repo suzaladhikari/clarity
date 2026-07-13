67ebb4ddf073fa0782e65638326f322b3f4e0b93
Peter <adhikarisuzal944@gmail.com>
2026-07-13 09:45:44 -0400

    started the garch training

--- src/api/predictivemodels.py
+++ src/api/predictivemodels.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from src.training.train_garch import params_garch
+from src.models.garch import Garch
+import pandas as pd
 
-print(params_garch)
-
+def return_value_by_ticker(ticker, datapath = './datas/combined_data.parquet'):
+     data = pd.read_par
