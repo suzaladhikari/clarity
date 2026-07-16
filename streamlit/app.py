a350896204a4f8f17fa3fad4740dd7feeae5b12a
Peter <adhikarisuzal944@gmail.com>
2026-07-16 11:06:12 -0400

    lets see if it works

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,2 +1,16 @@
 import streamlit as st
 st.title("Clarity: An AI-Powered Stock Volatility Forecasting Platform")
+
+st.header("What is Clarity")
+st.set_page_config(
+    page_title="Clarity",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("📈 Clarity")
+st.write(
+    "AI-powered stock volatility forecasting platform"
+)
+
+st.sidebar.success("Select a page above")
