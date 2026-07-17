fd5d00a2bffa17311ff236f555a8738cf7c01f0c
Peter <adhikarisuzal944@gmail.com>
2026-07-17 06:01:08 -0400

    all about ticker

--- streamlit/pages/1_📊_Predictor.py
+++ streamlit/pages/1_📊_Predictor.py
@@ -1,4 +1,7 @@
 
 import streamlit as st
+import requests
 
 st.header("Here you can predict the volatility for the next day using either of the four models: XGBoost (best peforming), LSTM, RNN, and Garch")
+
+st.subheader("Ticker: Ticker (or a ticker symbol) is a short, unique code used to identify a publicly traded company or asset in a stock exchange, currently we have 20 tickers, which you can predict volatility of.")
