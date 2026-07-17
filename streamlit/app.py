cf6289ae54c2500d29d48701bd7ce6a6431d5e7d
Peter <adhikarisuzal944@gmail.com>
2026-07-17 05:26:16 -0400

    faq 2

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 st.title("Clarity: A weather forecast for market turbulence")
 
-st.header("What is Clarity")
+st.header("What is Clarity ?")
 # st.set_page_config(
 #     page_title="Clarity",
 #     page_icon="📈",
 #     layout="wide"
 # )
-st.subheader("Clarity does not try to predict where the market goes tomorow. What it predicts is how much is the stock likely to swing, in either direction. That's volatility and unlike the price detection it's actually forecastable")
+st.subheader("Clarity does not try to predict where the market goes tomorow. What it predicts is how much is the stock likely to swing, in either direction. That's volatility and unlike the price detection it's actually forecastable.")
+
+### FAQ 2 
 
 
+st.header("Why does it matter?")
+
+st.subheader("Volatilty is not a side stat - its the metric a lot of financial companies rely on. Option traders uses volatilty mostly to trade off the price contracts. Better volatility decisions leads to better decisions with money.")
