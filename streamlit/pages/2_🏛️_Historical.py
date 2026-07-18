20634dd2bec692c312319c61de45361305a085ac
Peter <adhikarisuzal944@gmail.com>
2026-07-18 15:44:06 -0400

    updated image

--- streamlit/pages/2_🏛️_Historical.py
+++ streamlit/pages/2_🏛️_Historical.py
@@ -1,2 +1,22 @@
 import streamlit as st
-st.header("Comparisons between the models")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.header("Analysis of the models")
+st.subheader("Metrics: Root Mean Squared Error(RMSE), Mean Absolute Eror(MAE), r2 Score")
+model_performance_data = pd.read_parquet("./datas/modelpeformance.parquet")
+### Comparison of model's performance
+fix, ax = plt.subplots(1,2,figsize = (15,10))
+def plottingmetrics(x,y, data, title, xlabel, ylabel, ylim1, ylim2, axes):
+    sns.barplot(x = x, y =y, data = data, hue = x, ax = axes)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_title(title)
+    axes.set_ylim(ylim1, ylim2)
+    for container in axes.containers:
+        axes.bar_label(container, fmt="%.4f", padding=3)
+
+plottingmetrics('model', 'mae', model_performance_data, 'Comparison of MAE Scores', 'Models', 'MAE Score', 0, 0.01, ax[0])
+plottingmetrics('model', 'rmse', model_performance_data, 'Comparison of RMSE Scores', 'Models', 'RMSE Score', 0, 0.01, ax[1])
+plt.tight_layout()
+plt.show()
